0629300d0f0c5ce2367d1bd26cc2483d2d7d99ca
derlung <zxxs1105@naver.com>
2024-06-24 16:02:12 +0900

    new file:   .vscode/settings.json 	new file:   reviewcollector/.gitignore 	new file:   reviewcollector/build.gradle 	new file:   reviewcollector/gradle/wrapper/gradle-wrapper.jar 	new file:   reviewcollector/gradle/wrapper/gradle-wrapper.properties 	new file:   reviewcollector/gradlew 	new file:   reviewcollector/gradlew.bat 	new file:   reviewcollector/settings.gradle 	new file:   reviewcollector/src/main/java/com/derlung/reviewcollector/ReviewcollectorApplication.java 	new file:   reviewcollector/src/main/resources/application.properties 	new file:   reviewcollector/src/test/java/com/derlung/reviewcollector/ReviewcollectorApplicationTests.java

---
+++ reviewcollector/src/main/java/com/derlung/reviewcollector/ReviewcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.derlung.reviewcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewcollectorApplication.class, args);
+	}
+
+}
