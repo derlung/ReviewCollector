6fd8cbd247956d717cc0dcaba89b6925b54888ea
derlung <zxxs1105@zxxs1105@naver.com>
2024-06-24 16:18:58 +0900

    소스트리 첫 커밋

---
+++ src/test/java/com/derlung/reviewcollector/ReviewcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derlung.reviewcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewcollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
